add8a027c52fab5b5ee459528a6189d3be009be9
AugustoLinhares <augusto@linhares.sc>
2020-10-07 19:52:19 -0300

    mod para localhost

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: `https://questao6-back.server.prototipos.xyz/`})
+const api = axios.create({ baseURL: `http://localhost:5000/`})
 
 export default api;
